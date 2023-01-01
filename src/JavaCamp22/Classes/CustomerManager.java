7196a57de65ea58fe4b62bc880c61cacda13f8b7
Furkan Veli Mart <furkanvelimart@gmail.com>
2023-01-01 21:33:42 +0300

    classes added

---
+++ src/JavaCamp22/Classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package JavaCamp22.Classes;
+
+public class CustomerManager {
+	public void Add() {
+		System.out.println("Customer added.");
+	}
+	
+	public void Remove() {
+		System.out.println("Customer deleted.");
+	}
+	
+	public void Update() {
+		System.out.println("Customer updated.");
+	}
+
+}
